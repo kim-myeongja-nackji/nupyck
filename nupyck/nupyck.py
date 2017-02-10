47ea1e5fe17af48838d7dc72c3ff50c4c638e1fe
Callista Bee <callista.lavender.bee@gmail.com>
2017-02-09 21:07:01 -0800

    add function to compute template/primer concs

--- nupyck/nupyck.py
+++ nupyck/nupyck.py
@@ -10,11 +10,13 @@ os.environ['NUPACKHOME'] = nupack_dir
 nupack = cdll.LoadLibrary(os.path.join(nupack_dir, 'nupack.so'))
 nupack.pfuncFull.restype = c_longdouble
 nupack.pfunc.restype     = c_longdouble
+nupack.WaterDensity.restype = c_double
 
 # boltzmann constant
 kB = 0.0019872041
 
 c_array = lambda l: (c_int * len(l))(*l)
+c_double_array = lambda l: (c_double * len(l))(*l)
 
 baseToInt = dict( zip( "ACGT+" , [1,2,3,4,15] ) )
 def seqToInts(seq): return [ baseToInt[b] for b in seq ] + [-1]
@@ -53,6 +55,50 @@ def pfunc(seq_as_ints, temp, na=1.0, mg=0.0, freeEnergy = False):
     c_int(0)             # uselongsalt
   )
   return pf if not freeEnergy else -kB * (273.15+temp) * math.log(pf)
+
+def tp_concs(template, primer, t_conc, p_conc, temp):
+
+  t  = seqToInts(template)
+  p  = seqToInts(primer)
+  tt = seqToInts("+".join((template, template)))
+  tp = seqToInts("+".join((template, primer)))
+  pp = seqToInts("+".join((primer, primer)))
+
+  get_energy = lambda s: -math.log(pfunc(s, temp))
+  waterDensity = nupack.WaterDensity(c_double(temp))
+
+  # result array
+  x = c_double_array([-1,-1,-1,-1,-1])
+
+  A = (POINTER(c_int) * 2)(
+    c_array([1, 0, 2, 1, 0]),
+    c_array([0, 1, 0, 1, 2])
+  )
+  G            = c_double_array(map(get_energy, [t, p, tt, tp, pp]))
+  x0           = c_double_array([ t_conc/waterDensity, p_conc/waterDensity ])
+  numSS        = c_int(2)
+  numTotal     = c_int(5)
+  maxIters     = c_int(10000)
+  tol          = c_double(1e-7)
+  deltaBar     = c_double(1000)
+  eta          = c_double(0.125)
+  kT           = c_double((temp + 273.15) * kB)
+  maxNoStep    = c_int(50)
+  maxTrial     = c_int(100000)
+  perturbScale = c_double(100)
+  quiet        = c_int(0)
+  writeLogFile = c_int(0)
+  logFile      = c_char_p(None)
+  h20Density   = c_double(waterDensity)
+  seed         = c_int(0)
+
+  converged = nupack.CalcConc(
+    x, A, G, x0, numSS, numTotal, maxIters, tol, deltaBar,
+    eta, kT, maxNoStep, maxTrial, perturbScale, quiet, writeLogFile,
+    logFile, h20Density, seed
+  )
+
+  return np.array(x)
 
 def ppairs(seq_as_ints, temp):
   pairPr = POINTER(c_longdouble).in_dll(nupack, "pairPr")
