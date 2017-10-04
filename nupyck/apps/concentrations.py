6a4927e679da8404d4135f9dc985b5e764246d07
Callista Bee <callista.lavender.bee@gmail.com>
2017-10-04 10:35:41 -0700

    factor out concentration calculator

--- nupyck/apps/concentrations.py
+++ nupyck/apps/concentrations.py
@@ -35,63 +35,67 @@ def melting_temp(template, primer, t_conc, p_conc, t_lo, t_hi, na=1.0, mg=0.0,
 
     return temp
 
+
 def tp_fraction(template, primer, t_conc, p_conc, temp, na=1.0, mg=0.0):
-  """Return the fraction of template converted to template/primer duplex"""
+    """Return the fraction of template converted to template/primer duplex"""
 
-  t  = template
-  p  = primer
-  tt = "+".join((template, template))
-  tp = "+".join((template, primer))
-  pp = "+".join((primer, primer))
+    x0 = np.array([t_conc, p_conc])
 
-  kT = (temp + 273.15) * core.kB
+    G = np.array(
+            [ pfunc.pfunc(
+                [ template, primer ],
+                temp = temp, na = na, mg = mg,
+                material = core.DNA,
+                perm = p
+              )['energy'] for p in [1], [2], [1,1], [1,2], [2,2]
+            ]
+          )
 
-  G = [ pfunc.single(
-          seq, temp, material = core.DNA, na = na, mg = mg, symmetry = sym
-        )['energy'] / kT
-        for seq, sym in zip([ t, p, tt, tp, pp ], [ 1, 1, 2, 1, 2 ])
-      ]
+    A  = np.array([[1, 0, 2, 1, 0], [0, 1, 0, 1, 2]])
 
-  waterDensity = core.nupack.WaterDensity(core.c_double(temp))
+    x = calc_conc(x0, G, A, temp)
+    tp_conc = x[3]
+    frac = tp_conc / t_conc
 
-  # result array
-  x = core.c_double_array([-1,-1,-1,-1,-1])
+    return frac
 
-  A = (core.POINTER(core.c_int) * 2)(
-    core.c_array([1, 0, 2, 1, 0]),
-    core.c_array([0, 1, 0, 1, 2])
-  )
-  G            = core.c_double_array(G)
-  x0           = core.c_double_array([ t_conc/waterDensity, p_conc/waterDensity ])
-  numSS        = core.c_int(2)
-  numTotal     = core.c_int(5)
-  maxIters     = core.c_int(10000)
-  tol          = core.c_double(1e-7)
-  deltaBar     = core.c_double(1000)
-  eta          = core.c_double(0.125)
-  kT           = core.c_double(kT)
-  maxNoStep    = core.c_int(50)
-  maxTrial     = core.c_int(100000)
-  perturbScale = core.c_double(100)
-  quiet        = core.c_int(1)
-  writeLogFile = core.c_int(0)
-  logFile      = core.c_char_p(None)
-  h20Density   = core.c_double(waterDensity)
-  seed         = core.c_int(0)
 
-  converged = core.nupack.CalcConc(
-    x, A, G, x0, numSS, numTotal, maxIters, tol, deltaBar,
-    eta, kT, maxNoStep, maxTrial, perturbScale, quiet, writeLogFile,
-    logFile, h20Density, seed
-  )
+def calc_conc(x0, G, A, temp):
 
-  if not converged:
-    raise RuntimeError("concentration calculation did not converge")
+    numSS, numTotal = A.shape
+    kT = (273.15 + temp) * core.kB
+    waterDensity = core.nupack.WaterDensity(core.c_double(temp))
 
-  # convert back to molarity
-  x = np.array(x) * waterDensity
+    x = core.c_double_array([-1] * numTotal)
+    A            = (core.POINTER(core.c_int) * numSS)(
+                       *[ core.c_array(a) for a in A ]
+                   )
+    G            = core.c_double_array(G / kT)
+    x0           = core.c_double_array(x0 / waterDensity)
+    numSS        = core.c_int(numSS)
+    numTotal     = core.c_int(numTotal)
+    maxIters     = core.c_int(10000)
+    tol          = core.c_double(1e-7)
+    deltaBar     = core.c_double(1000)
+    eta          = core.c_double(0.125)
+    kT           = core.c_double(kT)
+    maxNoStep    = core.c_int(50)
+    maxTrial     = core.c_int(100000)
+    perturbScale = core.c_double(100)
+    quiet        = core.c_int(1)
+    writeLogFile = core.c_int(0)
+    logFile      = core.c_char_p(None)
+    h20Density   = core.c_double(waterDensity)
+    seed         = core.c_int(0)
 
-  # final [template-primer duplex] vs initial [template]
-  conc_ratio = x[3] / t_conc
-  return conc_ratio
+    converged = core.nupack.CalcConc(
+      x, A, G, x0, numSS, numTotal, maxIters, tol, deltaBar,
+      eta, kT, maxNoStep, maxTrial, perturbScale, quiet, writeLogFile,
+      logFile, h20Density, seed
+    )
 
+    if not converged:
+      raise RuntimeError("concentration calculation did not converge")
+
+    x = np.array(x) * waterDensity
+    return x
